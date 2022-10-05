6d09ad3d15ed184b929492af58a1e12dd9ec09c4
luciana-manuali <103532037+luciana-manuali@users.noreply.github.com>
2022-10-05 13:24:44 -0300

    make it better

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ exchange_rate = exchange_rate.round(2)
 exchange_rate.set_index(['Date'],inplace=True)
 exchange_rate.index = pd.to_datetime(exchange_rate.index)
 
+spread = pd.DataFrame()
+spread['MEP Spread'] = ((exchange_rate['MEP']/exchange_rate['Official'])-1)*100
+spread['CCL Spread'] = ((exchange_rate['CCL']/exchange_rate['Official'])-1)*100
+
 # Colors
 
 color_1 = '#641E16'
@@ -220,6 +224,83 @@ app.layout = html.Div(
             ],
             className="page",
         ),
+        # Page 3
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.Div(
+                                    [
+                                        html.Strong(
+                                            "Figure 2: MEP Spread and CCL Spread",
+                                        ),
+                                    ],
+                                    style={'width': '90%', 'display': 'inline-block', 'vertical-align': 'center',
+                                           'color': color_1},
+                                ),
+                                html.Div(
+                                    [
+                                        html.Img(
+                                            src=app.get_asset_url(
+                                                "logo_.png"
+                                            ),
+                                            style={'opacity': '0.2'},
+                                            className='page-1a',
+                                        ),
+                                    ],
+                                    style={'width': '10%', 'display': 'inline-block', 'vertical-align': 'center',
+                                           'text-align': 'right', 'color': 'black'},
+                                ),
+                            ],
+                            className="row",
+                        ),
+                        html.Div(
+                            [
+                                html.P(
+                                    "",
+                                ),
+                                dcc.DatePickerRange(
+                                    id="date-range2",
+                                    min_date_allowed=spread.index.min().date(),
+                                    max_date_allowed=spread.index.max().date(),
+                                    start_date=spread.index.min().date(),
+                                    end_date=spread.index.max().date(),
+                                    updatemode='singledate',
+                                )
+                                ,
+                                dcc.Graph(id="spread",
+                                          config={"displayModeBar": False},
+                                          ),
+                            ],
+                            className="thirdPage first row",
+                        ),
+                        html.Div(
+                            [
+                                html.Div(
+                                    [
+                                        html.H1("SPACE"),
+                                    ],
+                                    style={'width': '95%', 'display': 'inline-block', 'vertical-align': 'center',
+                                           'color': 'white'},
+                                ),
+                                html.Div(
+                                    [
+                                        html.H6("2"),
+                                    ],
+                                    style={'width': '5%', 'display': 'inline-block', 'vertical-align': 'center',
+                                           'text-align': 'right', 'color': 'black'},
+                                ),
+                            ],
+                            className="row",
+                        ),
+                    ],
+                    className="subpage",
+                )
+            ],
+            className="page",
+        ),
     ]
 )
 
@@ -312,6 +393,95 @@ def historica_perf(start_date, end_date):
     )
 
     return fig1
+
+@app.callback(
+    Output("spread", "figure"),
+    [
+        Input("date-range2", "start_date"),
+        Input("date-range2", "end_date"), ],
+)
+def historica_perf(start_date, end_date):
+    mask = (spread.index >= pd.to_datetime(start_date)) & \
+           (spread.index <= pd.to_datetime(end_date))
+
+    filtered_data = spread.iloc[mask]
+
+    fig2 = go.Figure()
+    for i, color in zip(filtered_data.columns, colors):
+        fig2.add_trace(go.Scatter(x=filtered_data[i].index,
+                                  y=filtered_data[i],
+                                  mode='lines',
+                                  connectgaps=True,
+                                  name=i,
+                                  line=dict(
+                                      color=color
+                                  )
+                                  )
+                       )
+    fig2.update_layout(
+        xaxis=dict(
+            showgrid=True,
+            showline=True,
+            zeroline=True,
+            gridcolor="lightgray",
+            zerolinecolor="lightgray",
+            showticklabels=True,
+            tickformat="%m-%Y",
+            linecolor="lightgray",
+            tickcolor="lightgray",
+            tickangle=-90,
+            ticks='outside',
+            type='date',
+            tickfont=dict(
+                family='Arial',
+                size=12,
+            ),
+            mirror=True,
+        ),
+        yaxis=dict(
+            showgrid=True,
+            showline=True,
+            showticklabels=True,
+            zeroline=True,
+            zerolinecolor="lightgray",
+            gridcolor="lightgray",
+            linecolor="lightgray",
+            tickcolor="lightgray",
+            titlefont=dict(
+                family='Arial',
+                size=12,
+            ),
+            tickfont=dict(
+                family='Arial',
+                size=12,
+            ),
+            mirror=True,
+            side='right',
+        ),
+        margin=dict(
+            r=10,
+            t=5,
+            b=0,
+            l=40,
+            pad=2,
+        ),
+        legend=dict(
+            yanchor="top",
+            y=1.3,
+            xanchor="center",
+            x=0.55,
+            orientation="h",
+            font=dict(
+                family='Arial',
+                size=12,
+            ),
+        ),
+        height=450,
+        showlegend=True,
+        plot_bgcolor='rgba(0,0,0,0)',
+    )
+
+    return fig2
 
 if __name__ == "__main__":
     app.run_server(debug=True)
